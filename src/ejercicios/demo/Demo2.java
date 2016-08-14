1cec9cf2dc6f90a439ff748b9f54627ada1cf7f3
Daniela-B <cendrabarbara@gmail.com>
2016-08-14 17:13:40 -0300

    Update Demo2.java
    Puse la resolución en el mismo archivo

--- src/ejercicios/demo/Demo2.java
+++ src/ejercicios/demo/Demo2.java
@@ -6,6 +6,21 @@ public class Demo2
 	 * Devuelve verdadero si todos los elementos están en el conjunto.
 	 */
 	public static boolean pertenecenTodos(int[] elems, int[] conjunto) {
+		if(elems.length>conjunto.length)
+			return false;
+		if(elems.length==0 || conjunto.length==0)
+			return false;
+		boolean acum=true;
+		for(int i=0;i<elems.length;i++){
+			acum=acum && siEsta(elems[i],conjunto);
+		}
+		return acum;
+	}
+	public static boolean siEsta(int elem, int[] arr){
+		for(int i=0;i<arr.length;i++){
+			if(arr[i]==elem)
+				return true;
+		}
 		return false;
 	}
 
@@ -20,10 +35,21 @@ public class Demo2
 	 * pico y, por tanto, al menos 3 elementos.
 	 */
 	public static int indicePico(int[] arreglo) {
+		int pos=-1;
+		int pico=arreglo[0];
+		if(arreglo.length==0)
+			return pos;
+		for(int i=1;i<arreglo.length;i++){
+			if(pico<arreglo[i]){
+				pico=arreglo[i];
+				pos=i;
+			}
+		}
+		return pos;
 		// Ejemplos:
 		//     [2, 4, 6, 19, 15, 8, -2] → se devuelve 3
 		//     [10, 20, 30, 40, 50, 15] → se devuelve 4
 		//     [50, 100, 75]            → se devuelve 1
-		return -1;
+	
 	}
 }
